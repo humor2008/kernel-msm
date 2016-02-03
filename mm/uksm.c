cc6e1810c84cd100f00617e4878d36c38003e39a
Alok Nikhil <nikhil.jan93@ufl.edu>
2016-02-03 19:52:17 +0530

    Revert "mm: UKSM: Fix build errors"
    This reverts commit ed25b64c9598d7231a834b070bf9c03b587d9535.

--- mm/uksm.c
+++ mm/uksm.c
@@ -164,7 +164,7 @@ static int is_full_zero(const void *s1, size_t len)
 
 #endif
 #else
-static int is_full_zero(void *s1, size_t len)
+static int is_full_zero(const void *s1, size_t len)
 {
 	unsigned long *src = s1;
 	int i;
@@ -180,6 +180,7 @@ static int is_full_zero(void *s1, size_t len)
 }
 #endif
 
+#define U64_MAX		(~((u64)0))
 #define UKSM_RUNG_ROUND_FINISHED  (1 << 0)
 #define TIME_RATIO_SCALE	10000
 
@@ -1603,7 +1604,7 @@ static inline int check_collision(struct rmap_item *rmap_item,
 static struct page *page_trans_compound_anon(struct page *page)
 {
 	if (PageTransCompound(page)) {
-		struct page *head = compound_head(page);
+		struct page *head = compound_trans_head(page);
 		/*
 		 * head may actually be splitted and freed from under
 		 * us but it's ok here.
@@ -4713,8 +4714,7 @@ out:
 	return referenced;
 }
 
-int try_to_unmap_ksm(struct page *page, enum ttu_flags flags,
-			struct vm_area_struct *target_vma)
+int try_to_unmap_ksm(struct page *page, enum ttu_flags flags)
 {
 	struct stable_node *stable_node;
 	struct node_vma *node_vma;
